973c93c634e199adc02ebb0246821384429eff50
John Kirrane <jkirrane@gmail.com>
2020-08-14 10:54:56 -0400

    remove x-api-key middleware for demo purposes

--- routes/zipcodes.js
+++ routes/zipcodes.js
@@ -2,20 +2,20 @@ var express = require('express');
 var router = express.Router();
 var zipcodes = require('zipcodes');
 
-router.use(function (req, res, next) {
-  console.log(req.headers)
-  if(req.headers['x-api-key'] == 'PAfWNRWCp43Wy1C9zj') {
-    next()
-  }
-  else {
-    return res.status(401).send({
-      "error": {
-        "code": "INVALID_AUTHORIZATION_CODE",
-        "message": "Invalid authorization code"
-      }
-    });
-  }
-})
+// router.use(function (req, res, next) {
+//   console.log(req.headers)
+//   if(req.headers['x-api-key'] == 'PAfWNRWCp43Wy1C9zj') {
+//     next()
+//   }
+//   else {
+//     return res.status(401).send({
+//       "error": {
+//         "code": "INVALID_AUTHORIZATION_CODE",
+//         "message": "Invalid authorization code"
+//       }
+//     });
+//   }
+// })
 
 /* GET zipcodes listing distances */
 router.get('/:zipcode1/:zipcode2', function(req, res, next) {
